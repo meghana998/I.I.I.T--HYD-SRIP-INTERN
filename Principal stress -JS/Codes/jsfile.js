861788ab060aa036a882399fa5ed5b4ce425d984
meghana998 <matta.meghana@gmail.com>
2019-09-28 19:33:25 +0530

    Add files via upload

---
+++ Principal stress -JS/Codes/jsfile.js
@@ -0,0 +1,9 @@
+var slider1 = document.getElementById("myRange1");
+			var output1 = document.getElementById("demo1");
+			var out1 = document.getElementById("d1");
+			out1.innerHTML = slider1.value;
+			output1.innerHTML = slider1.value;
+			slider1.oninput = function() {
+ 				 output1.innerHTML = this.value;
+ 				 out1.innerHTML = this.value;
+ 				 }
